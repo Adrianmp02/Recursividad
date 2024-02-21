6b6db0aa2ae37a63184286a1cf8ac0131387b9b1
Adrianmp02 <adrian.marper.6@educa.jcyl.es>
2024-02-21 12:51:22 +0100

    sdfg

--- Recursividad/src/es/juana/daw/Recursividad.java
+++ Recursividad/src/es/juana/daw/Recursividad.java
@@ -1,5 +1,21 @@
 package es.juana.daw;
 
 public class Recursividad {
-
+	
+	public static void main(String[] args) {
+		System.out.println(factorialNNumeros(5));
+	}
+	
+	
+	public static int sumarNPrimerosNumeros(int n) {
+		if(n == 1) return 1;
+		System.out.println("Suma de "+n+" mas los "+(n-1)+" primeros");
+		return n + sumarNPrimerosNumeros(n-1);
+	}
+	
+	public static int factorialNNumeros(int n) {
+		if(n == 1) return 1;
+		System.out.println("Multiplicacion de "+n+" mas los "+(n-1)+" siguientes");
+		return n * factorialNNumeros(n-1);
+	}
 }
